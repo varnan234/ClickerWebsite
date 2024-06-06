1d962d785901219ec4af319b0bc6d4f32d858ee6
Varnan Goenka <79697030+varnan234@users.noreply.github.com>
2024-06-06 18:27:46 -0400

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var element = document.getElementById("No");
 
 // The moving and changing text along with some like just saying stuff
-let texts = ["pakka?", "Plez", "100% pakka?", "esa mat kro na :(", "I'll kiss you", "Pls", "Maybe press the other one", "Dang I don't have enough prompts", "Please press the other one", "Ideas running low", "I want you :(", "One more chance", "last chance", "You thought", "Jk this is last chance", "Plsplspslpsl", "I'll be very sad", "pls say yes", "I'm begging", ":(((", "have mercy", "god will bless u", "PLEASE", "DDDDDDDDD:", "*sobs*", "pwease pookie bear", "WIFEEYYY PLZZZ", "😩", "or else(?)", "peewoop", "xoxo gossip gurl", "ill buy you shawarma deary", "i got $$$", "my mom will love you", "ill give u the lasT PIECE OF MY PANEER TIKKA", "niacinamide", "nitroglycerin", "wow",, "you must rlly hate me", "i promise we'll be good together"];
+let texts = ["Wsg", "NAH WSG?", "100% FR??", "IMMA CRY :(", "I'LL CALL YOU PAPI", "Pls", "Maybe press the other one", "Dang I don't have enough prompts", "Please press the other one", "Ideas running low", "BRUV CAP
+             ", "One more chance", "last chance", "You thought", "Jk this is last chance", "Plsplspslpsl", "I'll be very sad", "pls say yes", "I'm begging", ":(((", "have mercy", "god will bless u", "PLEASE", "DDDDDDDDD:", "*sobs*", "pwease pookie bear", "mannnn PLZZZ", "😩", "or else(?)", "peewoop", "xd xd pls", "ill buy you shawarma deary", "i got $$$", "my mom will love you", "ill give u the lasT PIECE OF MY PANEER TIKKA", "niacinamide", "nitroglycerin", "wow",, "you must rlly hate me", "i promise we'll be good together"];
 let index = 0;
 
 document.getElementById('theButton').addEventListener('click', function() {
